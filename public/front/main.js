8d622b7120eaddb94e4b9cd569138abae16163e8
Dmitry Turovtsov <web.best.master@gmail.com>
2017-08-23 22:07:58 +0300

    add nav-menu, swiper

--- public/front/main.js
+++ public/front/main.js
@@ -13,14 +13,52 @@ module.exports = __webpack_require__(2);
 "use strict";
 
 
-console.log('hello world!!');
 __webpack_require__(3);
+
+var homeScripts = __webpack_require__(5);
+
+window.addEventListener('load', function () {
+    homeScripts.initSwiper();
+});
 
 /***/ }),
 /* 3 */
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 4 */,
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var Swiper = __webpack_require__(4);
+
+module.exports.initSwiper = function () {
+    var swiperWrapperNode = window.document.querySelector('.js-home-swiper-wrapper.home-swiper-wrapper');
+
+    if (!swiperWrapperNode) {
+        console.log('swiper container node is not exist');
+        return;
+    }
+
+    var swiper = new Swiper(swiperWrapperNode, {
+        pagination: '.swiper-pagination',
+        // nextButton: '.swiper-button-next',
+        // prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        // spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: 6000,
+        autoplayDisableOnInteraction: false,
+        loop: true
+    });
+
+    console.log('swiper is here ->', swiper);
+};
 
 /***/ })
 ],[1]);
