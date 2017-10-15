2abf0954de28b095053785c1a412643aef54a699
Dmitry Turovtsov <web.best.master@gmail.com>
2017-10-15 13:12:27 +0300

    optimize some pages

---
+++ tools/tinify-external-product-image.js
@@ -0,0 +1,32 @@
+// h0DW7VyYVXnl3awj2o7v9wXR-EavOiB5 // done 7 march
+// h0DW7VyYVXnl3awj2o7v9wXR-EavOiB5 - kidmathgenius@gmail.com
+// eSu5nMg0TSDairQWQC_Bx0h41PxKgKEp - mikhail.anisimau.play@gmail.com
+// f8ZqkiaR5hwI9QRdc8Dwropue4kENmRp - dmitry.turovtsov@gmail.com
+// _JsmPE63lCa9UsS45vlKWMlhBhRntoK8 - logikaismekalka@gmail.com
+// uY9x_ytUQ0sq9-bB8iTvwGnmiWVci4an - web.best.master@gmail.com
+// RmSQIT1W2KC2_gZf27_KaZ7GWIzpmKJu - ae.fan.game@gmail.com
+
+const productsPath = './../public/external-storage/products/list';
+
+const tinify = require('tinify');
+tinify.key = 'f8ZqkiaR5hwI9QRdc8Dwropue4kENmRp';
+
+let ii = 400;
+
+let chain = Promise.resolve();
+
+for (; ii <= 550; ii += 1) {
+    (index => {
+        chain = chain.then(() => optimizeProduct(index)).then(() => console.log(index));
+    })(ii);
+}
+
+function optimizeProduct(ii) {
+    const {externalImages} = require(productsPath + '/' + ii + '/data.json');
+    return Promise.all(externalImages.map(imageName => optimizeImage(productsPath + '/' + ii + '/images/' + imageName)));
+}
+
+function optimizeImage(imagePath) {
+    return tinify.fromFile(imagePath).toFile(imagePath);
+}
+
---
+++ tools/tinify-internal-image.js
@@ -0,0 +1,43 @@
+// h0DW7VyYVXnl3awj2o7v9wXR-EavOiB5 // done 7 march
+// h0DW7VyYVXnl3awj2o7v9wXR-EavOiB5 - kidmathgenius@gmail.com
+// eSu5nMg0TSDairQWQC_Bx0h41PxKgKEp - mikhail.anisimau.play@gmail.com
+// f8ZqkiaR5hwI9QRdc8Dwropue4kENmRp - dmitry.turovtsov@gmail.com
+// _JsmPE63lCa9UsS45vlKWMlhBhRntoK8 - logikaismekalka@gmail.com
+// uY9x_ytUQ0sq9-bB8iTvwGnmiWVci4an - web.best.master@gmail.com
+// RmSQIT1W2KC2_gZf27_KaZ7GWIzpmKJu - ae.fan.game@gmail.com
+
+const fs = require('fs');
+
+const tinify = require('tinify');
+tinify.key = 'f8ZqkiaR5hwI9QRdc8Dwropue4kENmRp';
+
+let chain = Promise.resolve();
+
+function imageFilter(fileName) {
+    if (fileName.indexOf('.') === 0) {
+        return false;
+    }
+
+    return fileName.indexOf('.svg') === -1;
+}
+
+const productFolderImages = './../public/product/images';
+const categoryFolderImages = './../public/category/images';
+
+fs
+    .readdirSync(productFolderImages)
+    .filter(imageFilter)
+    .forEach(file => {
+        chain = chain.then(() => optimizeImage(productFolderImages + '/' + file));
+    });
+
+fs
+    .readdirSync(categoryFolderImages)
+    .filter(imageFilter)
+    .forEach(file => {
+        chain = chain.then(() => optimizeImage(categoryFolderImages + '/' + file));
+    });
+
+function optimizeImage(imagePath) {
+    return tinify.fromFile(imagePath).toFile(imagePath).then(() => console.log(imagePath));
+}
