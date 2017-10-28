eaf6f71fbfb78cbce574b17131262ed12a470b82
Dmitry Turovtsov <web.best.master@gmail.com>
2017-10-28 22:02:20 +0300

    update zip-site.sh

--- zip-size.sh
+++ zip-size.sh
@@ -2,6 +2,7 @@
 
 
 # this is work, --exclude=*.git* --exclude=*node_modules* - both will excluded
-zip -r --exclude=*.git* --exclude=*node_modules* site-`date +%Y-%m-%d-%H-%M-%S`.zip ./
+#zip -9 -r --exclude=*.git* --exclude=*.idea* --exclude=*node_modules* --exclude=*old-tb* --exclude=*_res* site-`date +%Y-%m-%d-%H-%M-%S`.zip ./
+zip -r --exclude=*.git* --exclude=*.idea* --exclude=*node_modules* --exclude=*old-tb* --exclude=*_res* site-`date +%Y-%m-%d-%H-%M-%S`.zip ./
 
 #zip -r site-`date +%Y-%m-%d-%H-%M-%S`.zip ./
