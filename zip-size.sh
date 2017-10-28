d72b6c024bdb7dbf16bedc6639daeed21bb088ae
webbestmaster <web.best.master@gmail.com>
2017-10-28 10:18:21 -0400

    update scripts

--- zip-size.sh
+++ zip-size.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-zip -r site-`date +%Y-%m-%d-%H-%M-%S`.zip ./
+
+# this is work, --exclude=*.git* --exclude=*node_modules* - both will excluded
+zip -r --exclude=*.git* --exclude=*node_modules* site-`date +%Y-%m-%d-%H-%M-%S`.zip ./
+
+#zip -r site-`date +%Y-%m-%d-%H-%M-%S`.zip ./
