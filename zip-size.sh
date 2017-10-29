1683ee8a6454766732faa974b436d9374bf660cf
dmitry.turovtsov <dmitry.turovtsov@synesis.ru>
2017-10-29 10:45:44 +0300

    remove extra files

--- zip-size.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-
-# this is work, --exclude=*.git* --exclude=*node_modules* - both will excluded
-#zip -9 -r --exclude=*.git* --exclude=*.idea* --exclude=*node_modules* --exclude=*old-tb* --exclude=*_res* site-`date +%Y-%m-%d-%H-%M-%S`.zip ./
-zip -r --exclude=*.git* --exclude=*.idea* --exclude=*node_modules* --exclude=*old-tb* --exclude=*_res* site-`date +%Y-%m-%d-%H-%M-%S`.zip ./
-
-#zip -r site-`date +%Y-%m-%d-%H-%M-%S`.zip ./
