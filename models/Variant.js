2d14f44f102dfe13f766b70926587c9b957d3e14
Dmitry Turovtsov <web.best.master@gmail.com>
2017-11-18 16:16:05 +0300

    add more products to Variant model

--- models/Variant.js
+++ models/Variant.js
@@ -15,42 +15,30 @@ const Variant = new keystone.List('Variant', {
     defaultSort: '-createdAt'
 });
 
-Variant.add({
+const variantSrc = {
     // product name, really this is ID
     name: {type: String, initial: true, required: true, index: true, 'default': ''},
 
     // name to show for user
-    displayName: {type: String},
+    displayName: {type: String}
+};
 
+// add products to variant
+let ii = 1;
+const maxProducts = 25;
+
+for (; ii < maxProducts; ii += 1) {
     // child products
-    product1: {
+    variantSrc['product' + ii] = {
         product: {type: Types.Relationship, ref: 'Product', many: false},
         label: {type: String}
-    },
-    product2: {
-        product: {type: Types.Relationship, ref: 'Product', many: false},
-        label: {type: String}
-    },
-    product3: {
-        product: {type: Types.Relationship, ref: 'Product', many: false},
-        label: {type: String}
-    },
-    product4: {
-        product: {type: Types.Relationship, ref: 'Product', many: false},
-        label: {type: String}
-    },
-    product5: {
-        product: {type: Types.Relationship, ref: 'Product', many: false},
-        label: {type: String}
-    },
-    product6: {
-        product: {type: Types.Relationship, ref: 'Product', many: false},
-        label: {type: String}
-    },
+    };
+}
 
-    // date of create product
-    createdAt: {type: Date, 'default': Date.now}
-});
+// date of create product
+variantSrc.createdAt = {type: Date, 'default': Date.now};
+
+Variant.add(variantSrc);
 
 // disable mongo db auto index
 // see https://github.com/keystonejs/keystone/wiki/Deployment-Checklist
