1294cb5d21c9ee71937cdc04e2c5ae6bcd2c80d5
webbestmaster <web.best.master@gmail.com>
2018-02-13 12:35:54 -0500

    move to bash

--- git-push.sh
+++ git-push.sh
@@ -10,7 +10,7 @@ echo "go to server repo"
 cd ~/apps/tattoobrands_by/
 
 echo "add all files to git"
-git add .
+git add --all
 
 currentDate=`date +%Y-%m-%d-%H-%M-%S`
 commitMessage="save state - $currentDate"
@@ -19,4 +19,4 @@ echo "commit $commitMessage"
 git commit -m "$commitMessage"
 
 echo "push to repo"
-git push
+sudo git push
