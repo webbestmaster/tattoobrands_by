d18a83aa14d1643af3f5995ae91f50d15a24695d
webbestmaster <web.best.master@gmail.com>
2018-02-13 11:54:32 -0500

    save state - 2018-02-13-11-54-32

--- back-up.sh
+++ back-up.sh
@@ -3,7 +3,10 @@
 echo "stop server"
 bash ./stop-server.sh
 
-echo "make zip back-up"
+echo "push into git"
+bash ./git-push.sh
+
+#echo "make zip back-up"
 zip -r --exclude=*.git* --exclude=*.idea* --exclude=*node_modules* --exclude=*old-tb* --exclude=*_res* tb-`date +%Y-%m-%d-%H-%M-%S`.zip ./apps/tattoobrands_by/
 
 echo "start server"
---
+++ git-push.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo "go to server repo"
+#cd ./apps/tattoobrands_by/
+
+echo "add all files to git"
+git add .
+
+currentDate=`date +%Y-%m-%d-%H-%M-%S`
+commitMessage="save state - $currentDate"
+
+echo "commit $commitMessage"
+git commit -m "$commitMessage"
+
+echo "push to repo"
+git push
