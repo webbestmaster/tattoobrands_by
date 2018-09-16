fd506db70ffefeae7056f737a9d28c07efcdf40f
dim <web.best.master@gmail.com>
2018-09-16 15:08:01 +0300

    setting - begin

---
+++ tools/updade-price.js
@@ -0,0 +1,56 @@
+const host = 'http://localhost:3000';
+const http = require('http');
+const fs = require('fs');
+
+async function request(url) {
+    return new Promise((resolve, reject) => {
+        http
+            .get(url, resp => {
+                let data = '';
+
+                // A chunk of data has been recieved.
+                resp.on('data', chunk => data += chunk);
+
+                // The whole response has been received. Print out the result.
+                resp.on('end', () => resolve(data));
+            })
+            .on('error', reject);
+    });
+}
+
+async function getSlugList() {
+    const allLinkData = JSON.parse(await request(host + '/api/get-all-links'));
+
+    return allLinkData.links.products
+        .filter(link => link.startsWith('/product/'))
+        .map(link => link.replace('/product/', ''));
+}
+
+async function getProductBySlug(slug) {
+    await new Promise(resolve => setTimeout(resolve, 1e3));
+
+    return JSON.parse(await request(host + '/api/get-product/' + slug));
+}
+
+async function writeAllProductListInFile() {
+    const slugList = await getSlugList();
+    const productList = [];
+    // const productListLength = slugList.length;
+    const productListLength = 10;
+    let ii = 0;
+
+    for (; ii < productListLength; ii += 1) {
+        productList.push(await getProductBySlug(slugList[ii]));
+        console.log(productList.length + ' / ' + productListLength);
+    }
+
+    fs.writeFile('product-list.json', JSON.stringify(productList), err => {
+        if (err) {
+            throw err;
+        }
+
+        console.log('The file has been saved!');
+    });
+}
+
+writeAllProductListInFile();
