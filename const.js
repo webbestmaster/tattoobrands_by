0ee32181004dde7c8c5bb86ec3a4fe4ea65988aa
dim <web.best.master@gmail.com>
2020-05-11 02:20:25 +0300

    make db dump limit

--- const.js
+++ const.js
@@ -7,7 +7,7 @@ module.exports.dataBaseConst = {
     url: 'mongodb://localhost:27001,localhost:27002,localhost:27003,localhost:27004?replicaSet=MyBestReplica',
     shallCommand: {
         backup: [
-            `mkdir ${databaseDumpFolderName};`,
+            `mkdir -p ${databaseDumpFolderName};`,
             'mongodump',
             '--port=27001',
             `--archive=${databaseDumpFolderName}/db-dump-\`date +%Y-%m-%d--%H-%M-%S\`.zip`,
